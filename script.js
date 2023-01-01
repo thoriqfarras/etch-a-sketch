ca9daa02ce14632749fac5b139ba6ae89620c8e9
Thoriq Farras <thoriqibrahimfarras@gmail.com>
2023-01-01 14:12:26 +0700

    adjusted class adding to new separated css classes

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 const grid = document.querySelector('#grid');
 
-let n = 2;
+let n = 5;
 let sq = n**2;
 let dimensionValue = `calc(500px / ${n})`;
 for (let i = 1; i <= sq; i++) {
     const tile = document.createElement('div');
     tile.classList.add('tile');
+    tile.classList.add('regular');
+
     if (i > sq - n) {
-        tile.classList.remove('tile');
-        tile.classList.add('tile-bottom');
+        tile.classList.remove('regular');
+        tile.classList.add('bottom');
     }
     if (i % n === 0) {
-        tile.classList.remove('tile');
-        tile.classList.add('tile-end');
+        tile.classList.remove('regular');
+        tile.classList.add('end');
     }
     if (i === sq) {
-        tile.classList.remove('tile-bottom');
-        tile.classList.remove('tile-end');
+        tile.classList.remove('bottom');
+        tile.classList.remove('end');
     }
+
     tile.style.width = dimensionValue;
     tile.style.height = dimensionValue;
     grid.appendChild(tile);
 }
+
