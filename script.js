c6c70fb033295dd5bc1041afb78a1eb4f6a8fecf
Thoriq Farras <thoriqibrahimfarras@gmail.com>
2023-01-02 18:51:37 +0700

    add canvas resize feature

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const grid = document.querySelector('#grid');
-
 function createGrid(n) {
     let sq = n**2;
     let dimensionValue = `calc(500px / ${n})`;
@@ -7,7 +5,7 @@ function createGrid(n) {
         const tile = document.createElement('div');
         tile.classList.add('tile');
         tile.classList.add('regular');
-
+        
         if (i > sq - n) {
             tile.classList.remove('regular');
             tile.classList.add('bottom');
@@ -20,11 +18,17 @@ function createGrid(n) {
             tile.classList.remove('bottom');
             tile.classList.remove('end');
         }
-
+        
         tile.style.width = dimensionValue;
         tile.style.height = dimensionValue;
         grid.appendChild(tile);
     }
+    return grid;
+}
+
+function removeGrid() {
+    const oldTiles = document.querySelectorAll('.tile');
+    for (let tile of oldTiles) grid.removeChild(tile);
 }
 
 function resetTile(tile) {
@@ -63,18 +67,28 @@ function updateTile(tile) {
     }
 }
 
-let mode = 'color';
+function listenForTileUpdates() {
+    let tiles = document.querySelectorAll('.tile');
+    resetBtn.addEventListener('click', () => { tiles.forEach(resetTile) });
+    tiles.forEach(tile => {
+        tile.addEventListener('mousedown', () => {
+            mouseDown = true; 
+            updateTile(tile);
+        });
+        tile.addEventListener('mousemove', () => { updateTile(tile)} );
+        tile.addEventListener('dragstart', (e) => { e.preventDefault(); });
+        window.addEventListener('mouseup', () => { if (mouseDown) mouseDown = false; });
+    });
+}
+
+// == BUTTONS ========================================================================================
+
 const buttons = document.querySelectorAll('button');
+let mode = 'color'; // color as default mode
 buttons.forEach(button => {
     button.addEventListener('click', updateActiveBtn);
 });
 
-const resetBtn = document.querySelector('#reset');
-resetBtn.addEventListener('click', () => {tiles.forEach(resetTile)});
-
-createGrid(64);
-
-const tiles = document.querySelectorAll('.tile');
 const colorPicker = document.querySelector('#color-picker');
 let mouseDown = false;
 let selectedColor = 'black'; // black as default value.
@@ -83,14 +97,26 @@ colorPicker.addEventListener('input', (event) => {
     selectedColor = event.target.value;
 });
 
-tiles.forEach(tile => {
-    tile.addEventListener('mousedown', () => {
-         mouseDown = true; 
-         updateTile(tile);
-    });
-    tile.addEventListener('mousemove', () => { updateTile(tile)} );
-    tile.addEventListener('dragstart', (e) => { e.preventDefault(); });
-    window.addEventListener('mouseup', () => { if (mouseDown) mouseDown = false; });
+const resetBtn = document.querySelector('#reset');
+
+const canvasResizer = document.querySelector('#canvas-resizer');
+let canvasSizeText = document.querySelector('#canvas-resizer p');
+console.log(canvasSizeText);
+
+// == GRID ========================================================================================
+
+
+let initialGridSize = document.querySelector('#canvas-resizer input').value;
+let currentGridSize = initialGridSize;
+let previousGridSize = 0;
+const grid = document.querySelector('#grid');
+createGrid(initialGridSize);
+
+canvasResizer.addEventListener('input', (e) => {
+    canvasSizeText.textContent = `${e.target.value} x ${e.target.value}`;
+    removeGrid();
+    createGrid(e.target.value);
+    listenForTileUpdates();
 });
 
-
+listenForTileUpdates();
