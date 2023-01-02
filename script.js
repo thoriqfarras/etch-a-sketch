e668944de0b281dfc19e58e417b52bbf18ec18ba
Thoriq Farras <thoriqibrahimfarras@gmail.com>
2023-01-02 11:41:42 +0700

    refactored grid creation and reset canvas into functions

--- script.js
+++ script.js
@@ -3,34 +3,40 @@ const grid = document.querySelector('#grid');
 let n = 32;
 let sq = n**2;
 let dimensionValue = `calc(500px / ${n})`;
-for (let i = 1; i <= sq; i++) {
-    const tile = document.createElement('div');
-    tile.classList.add('tile');
-    tile.classList.add('regular');
 
-    if (i > sq - n) {
-        tile.classList.remove('regular');
-        tile.classList.add('bottom');
-    }
-    if (i % n === 0) {
-        tile.classList.remove('regular');
-        tile.classList.add('end');
-    }
-    if (i === sq) {
-        tile.classList.remove('bottom');
-        tile.classList.remove('end');
-    }
+function createGrid() {
+    for (let i = 1; i <= sq; i++) {
+        const tile = document.createElement('div');
+        tile.classList.add('tile');
+        tile.classList.add('regular');
 
-    tile.style.width = dimensionValue;
-    tile.style.height = dimensionValue;
-    grid.appendChild(tile);
+        if (i > sq - n) {
+            tile.classList.remove('regular');
+            tile.classList.add('bottom');
+        }
+        if (i % n === 0) {
+            tile.classList.remove('regular');
+            tile.classList.add('end');
+        }
+        if (i === sq) {
+            tile.classList.remove('bottom');
+            tile.classList.remove('end');
+        }
+
+        tile.style.width = dimensionValue;
+        tile.style.height = dimensionValue;
+        grid.appendChild(tile);
+    }
 }
 
+function resetTile(tile) {
+    tile.style['background-color'] = 'white';
+}
+
+createGrid();
 const tiles = document.querySelectorAll('.tile');
 let mouseDown = false;
-
 tiles.forEach(tile => {
-
     tile.addEventListener('mousemove', () => {
         if (mouseDown) tile.style['background-color'] = 'black';
     });
@@ -49,7 +55,4 @@ tiles.forEach(tile => {
 });
 
 const resetBtn = document.querySelector('#reset');
-
-resetBtn.addEventListener('click', ()=>{
-    tiles.forEach(tile => { tile.style['background-color'] = 'white'; });
-});
+resetBtn.addEventListener('click', ()=>{tiles.forEach(resetTile)});
